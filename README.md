c6805cbdc63a2d7d5a7ea335a6e489c69e6ebd01
Brian Kropaczek <kropaczekb@gmail.com>
2022-10-29 11:34:10 -0400

    Testing framework in place

--- README.md
+++ README.md
@@ -3,5 +3,9 @@ THINGS TO DO TO SET UP:
     -Initialize db
     -Update db/index to reference db name
     -Set up models
+      -Include password hashing if necessary
   Set up routes:
     -api/index
+  Set up store
+  Set up components
+  Set up tests - INCLUDING package.json
--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,29 @@ const router = require('express').Router();
 
 router.use(/*ALL OTHER API ROUTES*/)
 
+//EXAMPLE LOGIN ROUTE FOR USERS
+router.post('/login', async (req, res, next) => {
+  try {
+    res.send({ token: await User.authenticate(req.body)});
+  } catch (err) {
+    next(err)
+  }
+})
+
+//EXAMPLE SIGNUP ROUTE FOR USERS
+router.post('/signup', async (req, res, next) => {
+  try {
+    const user = await User.create(req.body)
+    res.send({token: await user.generateToken()})
+  } catch (err) {
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.status(401).send('User already exists')
+    } else {
+      next(err)
+    }
+  }
+})
+
 router.use(function (req, res, next) {
   const err = new Error('Not found.');
   err.status = 404;
