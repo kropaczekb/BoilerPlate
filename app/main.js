5aec718d3830c2c2dba03d7b7d4c1fd3c33200de
Brian Kropaczek <kropaczekb@gmail.com>
2022-10-28 20:41:07 -0400

    Functional boilerplate with React

---
+++ app/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  // <div>Hello, world!</div>,
+  document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+);
