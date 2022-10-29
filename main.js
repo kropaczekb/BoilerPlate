4b1a9209a4d462b7908ad7de3c58cb723a6bf89a
Brian Kropaczek <kropaczekb@gmail.com>
2022-10-29 10:53:43 -0400

    Actual routes set up

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const app = require('./server')
-const PORT = 1337
+const PORT = process.env.PORT || 1337;
 
-    app.listen(PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
+app.listen(PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
